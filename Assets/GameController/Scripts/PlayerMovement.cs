b311d6d35f2f472b760496058f2c0c5a1bb9ade3
BenjaminLM95 <benjamin.lerner95@gmail.com>
2025-01-22 15:23:36 -0400

    Improving tilemap

--- Assets/GameController/Scripts/PlayerMovement.cs
+++ Assets/GameController/Scripts/PlayerMovement.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
     private Vector2 moveDirection = Vector2.zero; 
-    public float moveSpeed = 2f; 
-    
+    public float moveSpeed = 2f;
+
+
+    public GameObject spriteObject;
+    public SpriteRenderer spriteRenderer;
+    public Sprite UpSprite;
+    public Sprite DownSprite;
+    public Sprite LeftSprite;
+    public Sprite RightSprite;
+
     private void Awake()
     {
         playerRigidbody = this.GetComponent<Rigidbody2D>();
+        spriteRenderer = spriteObject.GetComponent<SpriteRenderer>();
         Actions.MoveEvent += UpdateMoveVector;
         
     }
@@ -18,11 +28,34 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         HandlePlayerMovement(); 
+
+
+        
+
+        if(moveDirection.y > 0) 
+        {
+            spriteRenderer.sprite = UpSprite;
+        }
+        else if(moveDirection.y < 0)
+        {
+            spriteRenderer.sprite = DownSprite;
+        }
+
+        if (moveDirection.x > 0)
+        {
+            spriteRenderer.sprite = RightSprite;
+        }
+        else if (moveDirection.x < 0)
+        {
+            spriteRenderer.sprite = LeftSprite;
+        }
+
     }
 
     private void UpdateMoveVector(Vector2 InputVector)
     {
         moveDirection = InputVector;
+        
 
     }
 
@@ -35,4 +68,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Actions.MoveEvent -= UpdateMoveVector; 
     }
+
+    
 }
